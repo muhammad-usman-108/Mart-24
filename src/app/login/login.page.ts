df317bf5605cfb4a3646c6c4396611b89480149e
Usman <muhammad.usman@ausy-technologies.de>
2022-04-16 00:43:07 +0200

    login app

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: 'login.page.html',
+  styleUrls: ['login.page.scss'],
 })
-export class HomePage {
+export class LoginPage {
 
   constructor() {}
 
